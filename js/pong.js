1c3d471a79f8094bf5d7158052fbce65875f65d2
Coutellier Romain <33906647+RedDarkS@users.noreply.github.com>
2020-11-03 10:24:05 +0100

    Update pong.js

--- js/pong.js
+++ js/pong.js
@@ -1,9 +1,11 @@
 
+//Déclaration de variables
 let largeur = $("#balle").width();
 let gauche = parseInt($("#balle").css("left"));
 let haut = parseInt($("#balle").css("top"));
 //alert(gauche);
 
+//boucle afin de modifier la position de la balle
 setInterval(function(){
     gauche = gauche+1;
     haut = haut+0.5;
